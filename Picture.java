30a36a1b777f823269b73dc77640a654cff128a1
CristianDAM <gutierrezperezcristian@gmail.com>
2016-10-13 11:58:33 +0200

    0982.c sol amarillo

--- Picture.java
+++ Picture.java
@@ -15,7 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
+   private Square floor;
   
 
     /**
@@ -51,18 +51,25 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("blue");
+        sun.changeColor("yellow");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
         
-        sun2 = new Circle();
-        sun2.changeColor("green");
-        sun2.moveHorizontal(150);
-        sun2.moveVertical(-40);
-        sun2.changeSize(80);
-        sun2.makeVisible();
+        
+        floor = new Square();
+        floor.changeColor("green");
+        floor.moveHorizontal(-310);
+        floor.moveVertical(140);
+        floor.changeSize(800);
+        floor.makeVisible();
+        
+        sun.slowMoveVertical(120);
+        
+        
+        
+        
     }
 
     /**
